aa1169a38e7f0aced97630f33aeb10df40d12e96
Alar31000 <lsfezza@gmail.com>
2021-11-28 14:17:36 -0500

    First Upload

---
+++ Main_page/Pompe2.vb
@@ -0,0 +1,14 @@
+﻿Public Class Pompe2
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Close()
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+End Class
